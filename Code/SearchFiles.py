a735c58aa0d54f41fc43b364073c6e8dd45bf4a4
Vathsavi Venkat <46141303+Vathsavi-Venkat@users.noreply.github.com>
2022-05-01 23:16:36 -0400

    Add files via upload

---
+++ Code/SearchFiles.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+INDEX_DIR = "Index.index"
+import sys, os, lucene
+ 
+from java.nio.file import Paths
+from org.apache.lucene.analysis.standard import StandardAnalyzer
+from org.apache.lucene.index import DirectoryReader
+from org.apache.lucene.queryparser.classic import QueryParser
+from org.apache.lucene.store import SimpleFSDirectory, FSDirectory
+from org.apache.lucene.search import IndexSearcher
+from java.io import File
+ 
+def run(searcher, analyzer):
+    while True:
+        print ("\nPress enter with no input to quit. \nIf field is blank, there is no data for it.")
+        command = input("Query: ")
+        if command == '':
+            return
+        print ("Searching for:", command)
+        query = QueryParser("contents", analyzer).parse(command)
+        scoreDocs = searcher.search(query, 50).scoreDocs
+        print ("%s total matching documents found." % len(scoreDocs))
+ 
+        for scoreDoc in scoreDocs:
+            doc = searcher.doc(scoreDoc.doc)
+            fullpath = os.path.join(doc.get("path")+'/'+doc.get("name"))
+            print ("\nLink to file", fullpath)
+ 
+if __name__ == '__main__':
+    lucene.initVM(vmargs=['-Djava.awt.headless=true'])
+    base_dir = os.path.dirname(os.path.abspath(sys.argv[0]))
+    directory = SimpleFSDirectory(Paths.get(os.path.join(base_dir, INDEX_DIR)))
+    searcher = IndexSearcher(DirectoryReader.open(directory))
+    analyzer = StandardAnalyzer()
+    run(searcher, analyzer)
+    del searcher
